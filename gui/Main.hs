437ff122bc05be2d38ba0113762379c2554acb8b
Ray Lehtiniemi <rayl@mail.com>
2013-07-21 13:46:54 -0600

    Initial display widget is a right-aligned label
    Signed-off-by: Ray Lehtiniemi <rayl@mail.com>

--- gui/Main.hs
+++ gui/Main.hs
@@ -13,6 +13,26 @@
 
 module Main (main) where
 
+import Graphics.UI.Gtk
+import Control.Monad.Trans(liftIO)
+
 main :: IO ()
-main = putStrLn "foo!"
+main = do
+   initGUI
+
+   window <- windowNew
+   window `on` deleteEvent $ liftIO mainQuit >> return False
+
+   vbox <- vBoxNew False 0
+   containerAdd window vbox
+
+   label <- labelNew (Just "ticker stream...")
+   miscSetAlignment label 1 0
+   boxPackStart vbox label PackGrow 0
+   labelSetJustify label JustifyRight
+   labelSetLineWrap label False
+
+
+   widgetShowAll window
+   mainGUI
 
