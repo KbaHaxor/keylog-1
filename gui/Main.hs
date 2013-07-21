5b3511472c42b964c133d81345ee2db884f07950
Ray Lehtiniemi <rayl@mail.com>
2013-07-21 11:34:42 -0600

    Skeleton of Haskell gui
    Signed-off-by: Ray Lehtiniemi <rayl@mail.com>

---
+++ gui/Main.hs
@@ -0,0 +1,18 @@
+-----------------------------------------------------------------------------
+-- |
+-- Module       : Main.hs
+-- Description  : XMonad keylogger gui
+-- Copyright    : (c) 2013 Ray Lehtiniemi
+-- License      : BSD3-style (see LICENSE)
+--
+-- Maintainer   : Ray Lehtiniemi <rayl@mail.com>
+-- Stability    : unstable
+-- Portability  : unportable
+--
+-----------------------------------------------------------------------------
+
+module Main (main) where
+
+main :: IO ()
+main = putStrLn "foo!"
+
