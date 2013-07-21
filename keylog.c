5e58fc35d6f9bd63418d2d31e4281ebb794da6bd
Ray Lehtiniemi <rayl@mail.com>
2013-07-20 19:39:27 -0600

    Parse cmdline
    Signed-off-by: Ray Lehtiniemi <rayl@mail.com>

--- keylog.c
+++ keylog.c
@@ -1,13 +1,94 @@
 
+#include <stdio.h>
+#include <stdlib.h>
 #include <getopt.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
 
 struct parms
 {
-	char *device;
-	char *keymap;
+	const char *device;
+	const char *keymap;
 };
+
+struct state
+{
+	int fd;
+};
+
+static struct parms parms =
+{
+	"/dev/input/event2",
+	NULL
+};
+
+static struct state state =
+{
+	0
+};
+
+static void die (const char *msg)
+{
+	perror(msg);
+	exit(-1);
+}
+
+static void usage (void)
+{
+	fprintf(stderr, "\nUsage: keylog [-d dev] [-k keymap]\n\n");
+	exit(-1);
+}
+
+static void parse_cmdline (int argc, char **argv)
+{
+	static struct option opts[] =
+	{
+	{ "device",         required_argument,   0,   'd' },
+	{ "keymap",         required_argument,   0,   'k' },
+	};
+
+	char c;
+
+	while ((c = getopt_long(argc, argv, "d:k:", opts, NULL)) != -1)
+	{
+		switch (c)
+		{
+		case 'd':
+			parms.device = optarg;
+			break;
+
+		case 'k':
+			parms.keymap = optarg;
+			break;
+
+
+		default:
+			usage();
+		}
+	}
+
+	if (optind < argc)
+	{
+		die("excess cmdline args");
+	}
+}
+
+static void open_keyboard (const struct parms *p, struct state *s)
+{
+	int fd;
+
+	if ((fd = open(p->device, O_RDONLY)) < 0)
+		die("open");
+
+	s->fd = fd;
+}
 
 int main (int argc, char **argv)
 {
+	parse_cmdline(argc, argv);
+	// parse keymap
+	open_keyboard(&parms, &state);
+	// loop processing events
 	return 0;
 }
