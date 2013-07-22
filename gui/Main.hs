df00a32e7f423debc185a2ef1e8640f4935285de
Ray Lehtiniemi <rayl@mail.com>
2013-07-21 19:15:40 -0600

    First simple stupid ticker display
    Signed-off-by: Ray Lehtiniemi <rayl@mail.com>

--- gui/Main.hs
+++ gui/Main.hs
@@ -13,8 +13,15 @@
 
 module Main (main) where
 
-import Graphics.UI.Gtk
+import System.Posix.Types
+import System.Posix.IO
 import Control.Monad.Trans(liftIO)
+import Graphics.UI.Gtk
+import Graphics.Rendering.Pango.Font
+import qualified System.Glib.MainLoop as M
+
+
+maxChars = 40
 
 main :: IO ()
 main = do
@@ -26,13 +33,34 @@ main = do
    vbox <- vBoxNew False 0
    containerAdd window vbox
 
+   font <- fontDescriptionFromString "courier bold 12"
+
    label <- labelNew (Just "ticker stream...")
    miscSetAlignment label 1 0
-   boxPackStart vbox label PackGrow 0
+   widgetModifyFont label (Just font)
+   labelSetSingleLineMode label True
+   labelSetMaxWidthChars label maxChars
    labelSetJustify label JustifyRight
    labelSetLineWrap label False
-
+   boxPackStart vbox label PackGrow 0
 
    widgetShowAll window
+
+   fd <- openFd "../key_stream" ReadWrite Nothing (OpenFileFlags False False False True False)
+   h <- M.inputAdd (fdFD fd) [IOIn] priorityDefault (readMore fd label)
+
    mainGUI
+
+fdFD :: Fd -> Int
+fdFD (Fd fd) = fromEnum fd
+
+readMore :: Fd -> Label -> IO Bool
+readMore fd l = do
+   s1 <- labelGetText l
+   (s2,n) <- fdRead fd 100
+   let x = s1 ++ s2
+       m = length x
+   labelSetText l $ drop (m - maxChars) x
+   widgetQueueDraw l
+   return True
 
